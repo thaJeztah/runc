5051011402d80f2d88952e5f550493f2e56679a4
Sebastiaan van Stijn <github@gone.nl>
2021-03-22 10:07:51 +0100

    libcontainer/system: remove UIDMapInUserNS and GetParentNSeuid
    - `UIDMapInUserNS()` is not used anywhere, only internally.
    - `GetParentNSeuid()` is not used by runc itself, and only used
      by a single external project
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libcontainer/system/userns_deprecated.go
+++ libcontainer/system/userns_deprecated.go
@@ -2,16 +2,7 @@ package system
 
 import "github.com/opencontainers/runc/libcontainer/userns"
 
-var (
-	// RunningInUserNS detects whether we are currently running in a user namespace.
-	// Originally copied from github.com/lxc/lxd/shared/util.go
-	// Deprecated: use github.com/opencontainers/runc/libcontainer/userns.RunningInUserNS
-	RunningInUserNS = userns.RunningInUserNS
-
-	// Deprecated: use github.com/opencontainers/runc/libcontainer/userns.UIDMapInUserNS
-	UIDMapInUserNS = userns.RunningInUserNS
-
-	// GetParentNSeuid returns the euid within the parent user namespace
-	// Deprecated: use github.com/opencontainers/runc/libcontainer/userns.RunningInUserNS
-	GetParentNSeuid = userns.RunningInUserNS
-)
+// RunningInUserNS detects whether we are currently running in a user namespace.
+// Originally copied from github.com/lxc/lxd/shared/util.go
+// Deprecated: use github.com/opencontainers/runc/libcontainer/userns.RunningInUserNS
+var RunningInUserNS = userns.RunningInUserNS
